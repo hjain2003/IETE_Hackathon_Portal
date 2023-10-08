617c32db50d5132e83bb1816ec3673a28eea23a8
Harsh <hjain_be21@thapar.edu>
2023-10-08 20:22:00 +0530

    added css styles

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { IoRocket } from 'react-icons/io5'; // Import the rocket icon
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -15,6 +16,21 @@ const Home = () => {
       <div className="full_page_home">
         <div className="home_div">
           <span className='heading_home'>Intra IETE Hackathon</span>
+          <br />
+          <span className='typeit'>
+          <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+
+                  .typeString("INVENT YOURSELF")
+
+                  .pauseFor(3000)
+                   .deleteAll()
+                   .typeString("INVENT YOURSELF")
+                  .start({loop:true});
+              }}
+            />
+          </span>
         </div>
         <br />
         <button className='arrow' onClick={navigateToPs}>
